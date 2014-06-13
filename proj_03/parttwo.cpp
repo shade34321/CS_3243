a6dacd2c801b47d0085a0f4dd182f57fa9be11d3
Shade Alabsa <shadea@ami.com>
2014-06-13 12:28:35 -0400

    Working on Part 2 and removed the two headers files since we don't need them.

--- proj_03/parttwo.cpp
+++ proj_03/parttwo.cpp
@@ -5,4 +5,33 @@
 // Date: 10 June 2014
 // File: parttwo.cpp
 
-#include "parttwo_h"
+// In general a point is in a circle if it meets the condition below
+// (x - center_x)^2 + (y - center_y)^2 < radius^2
+// where x and y are the points coordinates and center_x and center_y are the circles center point coordinates and radius is the radius of the circle.
+// If they are equal then it's a point on the circle which I will handle as being in the circle.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthreads.h>
+#include <time.h>
+
+using namespace std;
+
+double calculatePi(int, int)
+int getPoint();
+
+int main(){
+	srand(time(NULL));
+
+	return 0;
+}
+
+//Generate a number between 1 and 10 for  point.
+int getPoint() {
+	return (rand() % 10 + 1);
+}
+
+//calculates Pi based on how many hits we have
+double calculatePi(int hits, int darts) {
+	//pi = 4 * hits / total_darts.	return ((4*hits)/darts);
+}
--- proj_03/parttwo.h
+++
@@ -1,8 +0,0 @@
-// CS3243 Operating Systems
-// Summer 2014 
-// Project 3: Forks and Threads
-// Matthew Tinney and Shade Alabsa
-// Date: 10 June 2014
-// File: parttwo.h
-
-using namespace std;
