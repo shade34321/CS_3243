348dd0bd31f5a3697ce634aa83c6afaf039cc8b2
Shade Alabsa <sahde34321@gmail.com>
2014-06-17 19:51:40 -0400

    Moved partthree.cpp to partthree_ll.cpp

---
+++ proj_03/partthree_ll.cpp
@@ -0,0 +1,41 @@
+// CS3243 Operating Systems
+// Summer 2014 
+// Project 3: Forks and Threads
+// Matthew Tinney and Shade Alabsa
+// Date: 10 June 2014
+// File: partthree.cpp
+
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <pthread.h>
+#include <time.h>
+#include <vector>
+
+using namespace std;
+
+int importList(vector<int> *);
+
+int main(){
+
+	return 0;
+}
+
+int importList(vector<int> *list){
+	int data;
+	fstream f;
+
+	f.open("numbers.txt", fstream::in);
+
+	if (f.is_open()) {
+		while (f >> data) {
+			if (insert(list, list->head, data)) {
+				return -1;
+			}
+		}
+	}
+
+	return 0;
+}
