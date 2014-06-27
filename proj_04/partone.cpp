e9c6d6972f54fa3b7a080c86cc1d320c89a4812d
Shade Alabsa <shadea@ami.com>
2014-06-27 12:26:08 -0400

    Queue is working

--- proj_04/partone.cpp
+++ proj_04/partone.cpp
@@ -15,31 +15,119 @@
 
 using namespace std;
 
-int buffer[10];
+#define MAX_ELEMENTS 10
+
+struct node {
+	int data;
+	node *next;
+};
+
+struct queue{
+	int size;
+	int capacity;
+	node *buffer;
+	node *head;
+	node *tail;
+};
+
+queue *q;
 //pthread_mutex_t lock;
 
-void doWork();
-int insert_item(int *);
-int remove_item(int *);
+void push(int);
+void pop();
+void printQ();
+//void doWork();
+//int insert_item(int *);
+//int remove_item(int *);
 
 int main(){
 	srand(time(NULL));
 
+	q = (queue *)malloc(sizeof(queue));
+	q->buffer = (node *)malloc(sizeof(node)*MAX_ELEMENTS);
+	q->size = 0;
+	q->capacity = MAX_ELEMENTS;
+
+	q->head = NULL;
+	q->tail = NULL;
+
+	for (int i = 0; i < MAX_ELEMENTS+1; i++){
+		int r = rand();
+		printf("random number is %d\n", r);
+		push(r);
+	}
+
+	printQ();
+
+	for (int i = 0; i < MAX_ELEMENTS; i++){
+		pop();
+	}
+
+	pop();
+	
+	printQ();
 
 	return 0;
 }
 
+void push(int data){
+	if (q->size != q->capacity || q->size == 0) {
+		node *temp = (node *)malloc(sizeof(node));
+		temp->data = data;
+		if (q->size == 0){
+			q->head = temp;
+			q->tail = temp;
+		} else {
+			q->tail->next = temp;
+			q->tail = temp;
+		}
+
+		q->size++;
+	} else {
+		printf("The queue is full!\n");
+	}
+}
+
+void pop(){
+	if (q->size != 0){
+		node *temp = q->head;
+		q->size--;
+		q->head = temp->next;
+		free(temp);
+	} else {
+		printf("The queue is empty!\n");
+	}
+}
+
+void printQ(){
+	int i;
+
+	printf("Queue Capacity %d\nQueue Size %d\n", q->capacity, q->size);
+	
+	node *temp = q->head;
+	
+	for (i = 0; i < q->size; i++) {
+		printf("%d->", temp->data);
+		temp = temp->next;
+	}
+
+	printf("\n");
+}
+
+/**
 void consumer(){
+	if (buffer[0]){
+
+	}
+}
+
+void producer(){
 	int num = (rand() % 200 + 1);
 	if ((rand() % 20001 - 10000) % 3 == 0){
 		if (insert_item(num)){
 			cout << "Error inserting into buffer!" << endl;
 		}
 	}
-}
-
-void producer(){
-
 }
 
 int insert_item(int data){
@@ -49,3 +137,4 @@ int insert_item(int data){
 int remove_item(){
 
 }
+*/
